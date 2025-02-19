32e980c584daf14950c8d453062d2e7a805d6718
yimemories <94780705+adzka-sfr@users.noreply.github.com>
2025-02-20 06:50:22 +0700

    add link to register

--- auth/login.php
+++ auth/login.php
@@ -3,16 +3,17 @@
         <h4>Login</h4>
     </div>
     <div class="card-body">
-        <form action="login_process.php" method="POST">
-            <div class="form-group">
-                <label for="username">Username</label>
-                <input type="text" name="username" id="username" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="password">Password</label>
-                <input type="password" name="password" id="password" class="form-control" required>
-            </div>
-            <button type="submit" class="btn btn-primary btn-block">Login</button>
-        </form>
+        <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" id="username" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" class="form-control" required>
+        </div>
+        <button type="submit" class="btn btn-primary btn-block">Login</button>
+        <div class="form-group text-center mt-3">
+            <a href="main.php?page=register">New user? Register here</a>
+        </div>
     </div>
 </div>
