0756ba532fce183ccd4e9a26aba7d40cf653f8af
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-02-20 21:27:17 +0700

    w

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
-require_once '/home/u266480338/domains/adzkasfr.com/public_html/duit/config/connect.php';
-$user = require_once base_path('config/check_cookie.php');
-// Redirect to the dashboard if the user is authenticated
-echo "<script>window.location='" . base_url('dashboard') . "';</script>";
+// require_once '/home/u266480338/domains/adzkasfr.com/public_html/duit/config/connect.php';
+// $user = require_once base_path('config/check_cookie.php');
+// // Redirect to the dashboard if the user is authenticated
+// echo "<script>window.location='" . base_url('dashboard') . "';</script>";
+phpinfo();
