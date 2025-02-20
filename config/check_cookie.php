e1d9f10cbfe39e087fcebb12cb265763b6ea6275
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-02-20 21:13:02 +0700

    fix bug

--- config/check_cookie.php
+++ config/check_cookie.php
@@ -1,6 +1,6 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/duit/config/connect.php';
-require_once $_SERVER['DOCUMENT_ROOT'] . '/duit/assets/jwt/vendor/autoload.php';
+require_once __DIR__ . '/../config/connect.php';
+require_once __DIR__ . '/../assets/jwt/vendor/autoload.php';
 
 use \Firebase\JWT\JWT;
 use \Firebase\JWT\Key;
