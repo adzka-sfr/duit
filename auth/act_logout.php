8513bf0f308ddeea63094e503c784e0111a373c0
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-02-20 22:07:17 +0700

    f

--- auth/act_logout.php
+++ auth/act_logout.php
@@ -1,7 +1,7 @@
 <?php
 $cookie_name = "duit_token";
 $cookie_path = "/duit/"; // Must match the path used when setting the cookie
-$cookie_domain = ""; // Leave empty for localhost or set for production (e.g., "10.105.48.20")
+$cookie_domain = "duit.adzkasfr.com"; // Set for production
 
 // Unset the cookie from the PHP superglobal array
 unset($_COOKIE[$cookie_name]);
@@ -16,5 +16,5 @@ setcookie($cookie_name, "", time() - 3600, $cookie_path, "", false, true);
 setcookie($cookie_name, "", time() - 3600, "/", "", false, true);
 
 // Redirect to login page
-header("Location: /duit/auth/main.php?page=login");
+header("Location: /auth/main.php?page=login");
 exit();
