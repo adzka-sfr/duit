2417ea38d671f926d00afb361a3e4069491655de
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-02-20 21:08:08 +0700

    tes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
-require_once $_SERVER['DOCUMENT_ROOT'] . '/duit/config/connect.php';
+require_once 'config/connect.php';
 // $user = require_once base_path('config/check_cookie.php');
 // Redirect to the dashboard if the user is authenticated
 echo "tes";
