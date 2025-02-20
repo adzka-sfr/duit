0783ee0ffaf0e74926190085f362a395e29fc9a6
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-02-20 21:14:47 +0700

    tes

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 // ini_set('display_errors', 1);
 // ini_set('display_startup_errors', 1);
 // error_reporting(E_ALL);
-require_once __DIR__ . '/../config/connect.php';
-$user = require_once __DIR__ . '/../config/check_cookie.php';
+require_once '/config/connect.php';
+$user = require_once '/config/check_cookie.php';
 // Redirect to the dashboard if the user is authenticated
 echo "<script>window.location='" . base_url('dashboard') . "';</script>";
