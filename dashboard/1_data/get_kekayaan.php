e630bbc313d29667c7fa37ca7350459b9788c4b3
Adzka sfr <94780705+adzka-sfr@users.noreply.github.com>
2025-03-01 06:59:15 +0700

    fix bug

--- dashboard/1_data/get_kekayaan.php
+++ dashboard/1_data/get_kekayaan.php
@@ -23,36 +23,23 @@ if ($jwt === null) {
         $total_balance = $result ? $result['total_balance'] : 0;
 
         // Second query to get monthly comparison data
-        $stmt1 = $connect->prepare("SELECT 
-    -- Sum of all balances up to the last month
-    (SELECT COALESCE(SUM(c_balance), 0) 
-     FROM `v_monthly_balance` 
-     WHERE c_username = curr.c_username 
-     AND c_month <= :last_month) AS c_last_month_balance
-        FROM `v_monthly_balance` curr
-        WHERE curr.c_month = :month
-        AND curr.c_username = :username;
-        ");
+        $stmt1 = $connect->prepare("SELECT SUM(c_balance) AS c_last_month_balance
+                                    FROM `v_monthly_balance` 
+                                    WHERE c_username = curr.c_username 
+                                    AND c_month <= :last_month");
 
         $stmt1->bindParam(':username', $username, PDO::PARAM_STR);
         $stmt1->bindParam(':last_month', $last_month, PDO::PARAM_STR);
-        $stmt1->bindParam(':month', $month, PDO::PARAM_STR);
         $stmt1->execute();
         $last_month = $stmt1->fetch(PDO::FETCH_ASSOC);
         $last_month_balance = $last_month ? $last_month['c_last_month_balance'] : 0;
 
-        $stmt2 = $connect->prepare("SELECT 
-    (SELECT COALESCE(SUM(c_balance), 0) 
-    FROM `v_monthly_balance` 
-    WHERE c_username = curr.c_username 
-    AND c_month <= :month) AS c_this_month_balance
-        FROM `v_monthly_balance` curr
-        WHERE curr.c_month = :month
-        AND curr.c_username = :username;
-    ");
+        $stmt2 = $connect->prepare("SELECT SUM(c_balance) AS c_this_month_balance
+                                    FROM `v_monthly_balance` 
+                                    WHERE c_username = curr.c_username 
+                                    AND c_month <= :month");
 
         $stmt2->bindParam(':username', $username, PDO::PARAM_STR);
-        $stmt2->bindParam(':last_month', $last_month, PDO::PARAM_STR);
         $stmt2->bindParam(':month', $month, PDO::PARAM_STR);
         $stmt2->execute();
         $this_month = $stmt2->fetch(PDO::FETCH_ASSOC);
