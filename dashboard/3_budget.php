01454138f84f1c33ee93c1e343a83ec846b86c0e
yimemories <94780705+adzka-sfr@users.noreply.github.com>
2025-03-03 15:18:16 +0700

    tambah close button pada halaman budget

--- dashboard/3_budget.php
+++ dashboard/3_budget.php
@@ -28,6 +28,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title fs-5" id="exampleModalLabel">Buat anggaran - <b id="month-label"></b></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="row">
@@ -70,6 +71,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title fs-5" id="exampleModalLabel">Edit anggaran - <b id="month-label-edit"></b></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <div class="row">
